359b39f7a2ac9af5f578572d808e6f91a5ec8940
AslanAslanli <143632955+AslanAslanli@users.noreply.github.com>
2023-12-10 18:40:28 +0400

    first automation message

---
+++ src/test/java/com/loop/test/gitHubAutomation/GitHub_Login.java
@@ -0,0 +1,26 @@
+//package com.loop.test.gitHubAutomation;
+//
+//import com.loop.test.utilities.WebDriverFactory;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//
+//public class GitHub_Login {
+//    public static void main(String[] args) {
+//        WebDriver driver = WebDriverFactory.getDriver("chrome");
+//        driver.get("https://github.com/");
+//        driver.manage().window().maximize();
+//
+//
+//        WebElement signIn = driver.findElement(By.xpath("//[input[@href='/login']"));
+//        WebElement signIn = driver.findElement(By.xpath("//[input[@href='/login']"));
+//        signIn.click();
+//                //(By.xpath("//input[@value='java']"));
+//
+//
+//
+//
+//
+//    }
+//
+//}
